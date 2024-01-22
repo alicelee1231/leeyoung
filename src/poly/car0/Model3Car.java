75d077fde14d9d4b2638109270803a5d78362c65
alicelee1231 <saemmilee1231@gmail.com>
2024-01-23 06:35:10 +0900

    다형성 비교

---
+++ src/poly/car0/Model3Car.java
@@ -0,0 +1,14 @@
+package poly.car0;
+
+public class Model3Car {
+    public void startEngine(){
+        System.out.println("model3car.startEngine");
+    }
+    public void offEngine(){
+        System.out.println("model3car.offEngine");
+    }
+
+    public void pressAccelerator(){
+        System.out.println("model3car.pressAccelerator");
+    }
+}
